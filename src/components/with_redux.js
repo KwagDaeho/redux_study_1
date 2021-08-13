7e66d2dd4bbb32720d49c4df3fe4c675d41650ea
Haraho <1438eogh@gmail.com>
2021-08-13 23:17:35 +0900

    add:with redux

--- src/components/with_redux.js
+++ src/components/with_redux.js
@@ -1,9 +1,26 @@
 import react from "react";
+import { combineReducers, createStore } from "redux";
 
 const WithRedux = () => {
+  createStore();
+  let red = () => {
+    document.querySelector("#red_box").style.backgroundColor = "#f00";
+    document.querySelector("#green_box").style.backgroundColor = "#f00";
+  };
+  let green = () => {
+    document.querySelector("#red_box").style.backgroundColor = "#0f0";
+    document.querySelector("#green_box").style.backgroundColor = "#0f0";
+  };
   return (
     <div>
-      <h1></h1>
+      <div className="container" id="red_box">
+        <h1 className="red">red</h1>
+        <input type="button" value="fire" onClick={red}></input>
+      </div>
+      <div className="container" id="green_box">
+        <h1 className="green">green</h1>
+        <input type="button" value="fire" onClick={green}></input>
+      </div>
     </div>
   );
 };
